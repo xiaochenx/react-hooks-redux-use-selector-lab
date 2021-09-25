5bc631898a31ab95e99d791fbd4286d4804ae476
Xiaochen Xu <xiaochex@gmail.com>
2021-09-24 21:53:47 -0400

    hooked up redux store and successfully dispatched

--- src/index.js
+++ src/index.js
@@ -3,12 +3,20 @@ import ReactDOM from "react-dom";
 import App from "./App";
 
 import usersReducer from "./features/users/usersSlice";
-
+import { createStore } from "redux";
+import { Provider } from "react-redux";
 // add imports and code
+
+const store = createStore(
+  usersReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
 
 ReactDOM.render(
   // add imports and code
-  <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   // add imports and code
   document.getElementById("root")
 );
