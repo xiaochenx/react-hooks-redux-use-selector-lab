b81c38baef604f1f438d03e9e90f709710be9083
Xiaochen Xu <xiaochex@gmail.com>
2021-09-25 09:57:47 -0400

    minor update with userSelector hook

--- src/features/users/Users.js
+++ src/features/users/Users.js
@@ -1,14 +1,32 @@
 import React from "react";
+import { useSelector } from "react-redux";
 // add any needed imports here
 
 function Users() {
+  const users = useSelector((state) => state.users);
+  const userCount = useSelector((state) => state.users.length);
+  
   return (
+ 
     <div>
+    
       <ul>
         Users!
         {/* Write code here that displays the usernames of all users in the Redux store */}
         {/* In addition, display the total number of users curently in the store */}
+        
+
+        {users.map((user) => (
+          <li key={user.username}>
+            Name: {user.username}
+            <ul>
+              <li key={user.hometown}>Hometown:{user.hometown}</li>
+            </ul>
+          </li>
+         
+        ))}
       </ul>
+      Total Users: {userCount}
     </div>
   );
 }
